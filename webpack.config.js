095f69f651774b1b62b31fbd31509883d340bc91
annaebrown <annabrown4789@gmail.com>
2017-03-07 20:53:30 -0500

    boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/main.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  context: __dirname,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+ module: {
+     rules: [{
+       test: /jsx?$/,
+       exclude: /(node_modules|bower_components)/,
+       use: [{
+         loader: 'babel-loader',
+         options: {
+           'presets': ['react', 'es2015', 'stage-2']
+         }
+       }]
+     }]
+   }
+};
